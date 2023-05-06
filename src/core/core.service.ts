98b253ed33134247ce31f17add2d9617c3deaf04
AlbaSly <arias.brando.raxel@gmail.com>
2023-05-06 10:33:58 -0600

    Cambio temporal

--- src/core/core.service.ts
+++ src/core/core.service.ts
@@ -4,6 +4,9 @@ import { Ciudades } from 'src/db/airbus_380_acad/entities/Ciudades';
 import { DIRSHelper } from 'src/helpers/dirs.helper';
 import { Repository } from 'typeorm';
 
+/**TODO: Crear Patrón Repository para conexión SQL */
+import * as sql from "mssql";
+
 @Injectable()
 export class CoreService {
     constructor(
@@ -16,6 +19,12 @@ export class CoreService {
         const apellidosCSVPath: string = await DIRSHelper.getProjectCSVDIR("random_data_apellidos");
 
         const data = await this.CiudadesRepository.find({});
+
+        /**HACIENDO PRUEBA DE CONEXIÓN EN CRUDO */
+        console.log("Conectando a la base de datos random_data");
+        await sql.connect('Server=localhost,1433;Database=random_data;User Id=sa;Password=Albabianca15042002_;Encrypt=true;TrustServerCertificate=true')
+        const result = await sql.query`select * from apellidos`;
+        console.log(result);
 
         return data;
     }
